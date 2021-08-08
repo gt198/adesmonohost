74377c435b1191f4011d0310830624f370cdb2ac
gt198 <GABRIELTLH.20@ichat.sp.edu.sg>
2021-08-08 22:18:00 +0800

    Updates

---
+++ index.js
@@ -0,0 +1,20 @@
+const host = 'https://mastermind-ades.herokuapp.com/';
+
+window.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('start-game').addEventListener('click', function () {
+        document.getElementById('start-game').disabled = true;
+        fetch(`${host}/`, {method: 'POST'})
+        .then(function (response) {
+            document.getElementById('start-game').disabled = false;
+            return response.json();
+        })
+        .then(function (json) {
+            window.alert('Mastermind game ' + json.game_Id + ' has started');
+            localStorage["sessionId"] = json.game_Id;
+            console.log(localStorage["sessionId"]);
+            window.location.href = "./game.html"
+        })
+    })
+
+   
+});
