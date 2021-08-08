6b454a2a1522db3611b79d397f01f7253bcd6325
gt198 <GABRIELTLH.20@ichat.sp.edu.sg>
2021-08-08 22:19:41 +0800

    host frontend

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,4 @@
-const host = 'http://localhost:8080';
+const host = 'https://mastermind-ades.herokuapp.com/';
 
 window.addEventListener('DOMContentLoaded', function () {
     document.getElementById('start-game').addEventListener('click', function () {
